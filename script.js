6fcc606e600893bcbe3a5b25632887c74b126ca1
Jaysaha1401 <jaysaha135@gmail.com>
2026-08-11 15:13:06 +0530

    Added working product image carousel

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 const sliderImages = document.querySelectorAll(".product-slider img");
 
+const previousButton = document.querySelector(".slider-prev");
+const nextButton = document.querySelector(".slider-next");
+
 let currentImage = 0;
 
-setInterval(() => {
-    sliderImages[currentImage].style.display = "none";
+// Show only the current image
+function showImage(index) {
+    sliderImages.forEach((image, i) => {
+        image.style.display = i === index ? "block" : "none";
+    });
+}
 
+// Next image
+nextButton.addEventListener("click", () => {
     currentImage++;
 
     if (currentImage >= sliderImages.length) {
         currentImage = 0;
     }
 
-    sliderImages[currentImage].style.display = "block";
+    showImage(currentImage);
+});
+
+// Previous image
+previousButton.addEventListener("click", () => {
+    currentImage--;
+
+    if (currentImage < 0) {
+        currentImage = sliderImages.length - 1;
+    }
+
+    showImage(currentImage);
+});
+
+// Start with first image
+showImage(currentImage);
+
+// Automatic change every 3 seconds
+setInterval(() => {
+    currentImage++;
+
+    if (currentImage >= sliderImages.length) {
+        currentImage = 0;
+    }
+
+    showImage(currentImage);
 }, 3000);
